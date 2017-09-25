1e63e5fe7f2c863ae0023a0d39cbbb3201f4fc72
valbeat <kj1ktk@gmail.com>
2017-09-25 23:35:48 +0900

    [update] 定数はconstで宣言. iotaという文法がenumっぽい

--- hello.go
+++ hello.go
@@ -9,4 +9,16 @@ func main() {
 	var d bool
 	// データ型に応じたフォーマットを選択する
 	fmt.Printf("a: %d, b:%f, c:%s, d:%t\n", a, b, c, d)
+
+	// 定数はconst
+	const name = "takuma"
+	fmt.Println(name)
+
+	// iota = enum ?
+	const (
+		sun = iota // 0
+		mon
+		tue
+	)
+	fmt.Println(sun, mon, tue)
 }
