30107bcbedf7f4a18de3fd49ceef6440e6c0bcf8
valbeat <kj1ktk@gmail.com>
2017-10-01 23:55:39 +0900

    [update] レシーバをポインタで受け取るメソッドの例

--- struct.go
+++ struct.go
@@ -18,13 +18,20 @@ func newTask(id int, detail string) *Task {
 	return task
 }
 
+// レシーバをポインタで受け取る例
+func (task *Task) Finish() {
+	task.done = true
+}
+
+// レシーバを値で受け取る例
 // stringに変換するメソッド
 func (task Task) String() string {
-	str := fmt.Sprintf("%d:%s", task.ID, task.Detail)
+	str := fmt.Sprintf("[%t] %d:%s", task.done, task.ID, task.Detail)
 	return str
 }
 
 func main() {
 	task := newTask(1, "Goの練習")
+	task.Finish()
 	fmt.Println(task)
 }
