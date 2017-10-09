6228b0ec8a9e755ca692e0697ca7286b3d37301f
valbeat <kj1ktk@gmail.com>
2017-10-10 00:15:00 +0900

    [update] 新規作成

---
+++ multi.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"time"
+	"net/http"
+	"log"
+	"fmt"
+)
+
+// ステータスをチャネルとして取得するメソッド
+// 戻り値を<-chanと読み出し専用にすることで外部からの書き込みを防ぐ
+func getStatus(urls []string) <-chan string {
+	// バッファをURLの数(3)に
+	statusChan := make(chan string)
+	for _,url := range urls  {
+		// 処理を関数化し、goを付けると非同期処理となる
+		go func(url string) {
+			res, err := http.Get(url)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			// チャネルに書き込み
+			// mainの読み込みが遅くてもbufferがあるのですぐに終わる
+			statusChan <- res.Status
+		}(url)
+	}
+	return statusChan
+}
+
+func main() {
+	timeout := time.After(time.Second)
+	urls := []string{
+		"http://google.com",
+		"http://yahoo.com",
+		"http://exampl.com",
+	}
+	statusChan := getStatus(urls)
+	// 任意のラベル
+	LOOP:
+	for {
+		select {
+		case status := <- statusChan:
+			fmt.Println(status)
+		case <-timeout:
+			// for文から抜けるためにラベルを付ける
+			break LOOP
+		}
+	}
+}
